717522110a7767e240408cbb637fb85e0c4e7d17
Hugo <Hugo@DESKTOP-7K2S73S>
2020-12-21 23:49:37 +0100

    Création du dépôt

---
+++ src/demineur/Grille.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package demineur;
+
+/**
+ *
+ * @author Hugo
+ */
+public class Grille {
+int largeur;
+int longueur;
+Cellule[][] Cellules = new Cellule[largeur][longueur];
+
+ 
+}
