b6c9c6489627eb6b4754b52010df0105cc6f1c8c
Hugo <Hugo@DESKTOP-7K2S73S>
2021-01-01 13:40:46 +0100

    Création du dépôt

--- src/demineur/Grille.java
+++ src/demineur/Grille.java
@@ -13,6 +13,8 @@ public class Grille {
 int largeur;
 int longueur;
 Cellule[][] Cellules = new Cellule[largeur][longueur];
+int CaseDecouverte;
+int proxiMine;
 
 Grille() {
         for (int i=0; i<largeur; i++) {
@@ -23,7 +25,7 @@ Grille() {
         }
     }
 
-void activer_mine(int column) { // ressort vrai si il y a un desing
+void activer_mine(int column) { 
         int i = largeur-1;
         while (Cellules[i][column].mine == false) {
             i--;
@@ -62,16 +64,37 @@ int analyseMineAutour(int lignes, int colonnes) {
    return nbMines;
 }
     
-void ouvrirCellules(int lignes, int colonnes) {
+void decouvrirCellules(int lignes, int colonnes) {
     if (lignes >= 0 && lignes <largeur && colonnes >= 0 && colonnes < longueur && Cellules[lignes][colonnes].mine == false) {
         int mineAutour = analyseMineAutour(lignes, colonnes);
-        
+        CaseDecouverte ++;
+    
+     if (mineAutour == 0) {
+         CaseDecouverte = proxiMine;
+         decouvrirCellules(lignes, colonnes +1);
+         decouvrirCellules(lignes+1, colonnes);
+         decouvrirCellules(lignes+1, colonnes +1);
+         decouvrirCellules(lignes-1, colonnes);
+         decouvrirCellules(lignes, colonnes-1);
+         decouvrirCellules(lignes-1, colonnes-1);
+         decouvrirCellules(lignes+1, colonnes-1);
+         decouvrirCellules(lignes-1, colonnes+1);
+     }
     }
-    
-    
-    
 }
 
+void ouvrirCellules(int ligne, int colonne) {
+    while(ligne>=0 && ligne<largeur && colonne >=0 && colonne<longueur) {
+        if (Cellules[ligne][colonne].mine == false) {
+            
+        }
+    }
+                
+          
+            
+
+
+}    
 void viderGrille() {
         for (int i = 0; i < largeur; i++) {
             for (int j = 0; j < longueur; j++) {
@@ -83,7 +106,7 @@ void viderGrille() {
             }
         }
     
- boolean placerTrouNoir(int ligne, int colonne) {
+boolean placerTrouNoir(int ligne, int colonne) {
         if (!Cellules[ligne][colonne].mine) {
             Cellules[ligne][colonne].mine = true;
             return true;
@@ -91,5 +114,37 @@ void viderGrille() {
         return false;
     } 
 
+
+void afficherGrillesurConsole(){
+    for (int i = largeur-1; i >= 0; i--) {
+            for (int j = 0; j < longueur; j++) {
+                if(Cellules[i][j].mine){
+                    System.out.print("\u001B[0m O ");
+                }
+                  else if (Cellules[i][j].drapeau) {
+                    System.out.print("\u001B[0m D ");
+                } else if (Cellules[i][j].proxiMine ==1) {
+                    System.out.print("\u001B[0m N ");
+                
+                } else if (Cellules[i][j].kitDeDeminage) {
+                    System.out.print("\u001B[0m K ");
+                } 
+            }
+            System.out.println(" " + (i+1));
+        }
+        for(int i=0; i<longueur;i++){
+            System.out.print(" " + (i+1) + " ");
+        }
+        System.out.println();
 }
 
+boolean placerKit(int ligne, int colonne) {
+        if (!Cellules[ligne][colonne].kitDeDeminage) {
+            Cellules[ligne][colonne].kitDeDeminage = true;
+            return true;
+        }
+        return false;
+    }
+
+
+}
