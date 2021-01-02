886c3fd08b517149712777fb28cbe8b15ff1b37b
Hugo <Hugo@DESKTOP-7K2S73S>
2021-01-02 23:31:56 +0100

    Création du dépôt

--- src/demineur/Partie.java
+++ src/demineur/Partie.java
@@ -5,10 +5,54 @@
  */
 package demineur;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  *
  * @author Hugo
  */
 public class Partie {
-    
+Grille grilleJeu = new Grille();
+ 
+
+
+
+void initialiserPartie() {
+        //Mise en place de la grille
+        grilleJeu.viderGrille();
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Choix du pseudo du joueur :");
+        Joueur joueur = new Joueur(sc.nextLine());
+        
+        System.out.println("Choix du nombre de lignes");
+        int largeur = sc.nextInt();
+        System.out.println("Choix du nombre de colonnes");
+        int longueur = sc.nextInt();
+        
+        //générer mines
+        Random r = new Random();
+        int mineAleat = r.nextInt( - min + 1) + min;
+        
+        
+        int compteur = 0;
+        for (int i = 0; i < 5; i++) {
+            int ligne_mine = r.nextInt(largeur);
+            int colonne_mine = r.nextInt(longueur);
+            if (compteur < 2) {
+                if (!grilleJeu.placerMine(ligne_mine, colonne_mine)) {
+                    compteur--;
+                }
+                compteur = compteur + 1;
+            }
+            if (!grilleJeu.placerTrouNoir(ligne_trou_noir, colonne_trou_noir)) {
+                i--;
+            }
+        }
+
+
+        grilleDeJeu.afficherGrilleSurConsole();
+
+    }
 }
